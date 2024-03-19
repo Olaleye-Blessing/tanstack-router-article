14b3b4cf2205c4143216d1b33cbd1112c0114844
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-03-19 12:41:40 +0100

    Create about page

---
+++ src/routes/about.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+function About() {
+	return <main className='p-2'>Hello from About!</main>;
+}
+
+export const Route = createFileRoute('/about')({
+	component: About,
+});
